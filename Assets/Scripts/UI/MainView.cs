5e10f5a244472064b9d0fe3ee95860a7ef1cc378
KienPham <kien@playstudios.asis>
2026-03-25 21:21:48 +0700

    add apk

--- Assets/Scripts/UI/MainView.cs
+++ Assets/Scripts/UI/MainView.cs
@@ -4,17 +4,27 @@ using UnityEngine.UI;
 public class MainView : MonoBehaviour
 {
     [SerializeField] private Button upgradeButton;
+    [SerializeField] private Button addGoldButton;
     [SerializeField] private UpgradeView upgradeView;
 
     private void Awake()
     {
         if (upgradeButton != null)
             upgradeButton.onClick.AddListener(OnClickUpgrade);
+        
+        if (addGoldButton != null)
+            addGoldButton.onClick.AddListener(OnClickAddGold);
     }
 
     private void OnClickUpgrade()
     {
         if (upgradeView != null)
             upgradeView.Show();
+    }
+    
+    private void OnClickAddGold()
+    {
+        if (GameManager.Instance != null)
+            GameManager.Instance.AddGold(100000);
     }
 }
